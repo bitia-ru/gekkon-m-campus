9ad892aec4effbb7862171539694f8ac565e0fa6
Artem Levenkov <alevenkov@networkoptix.com>
2022-01-08 16:20:25 +0300

    CAMPM-7: Introduce user actions history page

--- src/v2/V2.jsx
+++ src/v2/V2.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import BootingScreen from './screens/BootingScreen';
 import { currentUser as currentUserObtainer } from './redux/user_session/utils';
 import SpotsShow from '@/v2/screens/SpotsShow';
 import SpotsIndex from '@/v2/screens/SpotsIndex';
+import { default as HistoryScreen } from './screens/History';
 import LoginVKError from '@/v2/components/LoginVK/LoginVKError';
 import LoginVKSuccess from '@/v2/components/LoginVK/LoginVKSuccess';
+import { SessionFetcher } from './fetchers/SessionFetcher';
+
 
 const V2 = ({ currentUser }) => (
   <>
@@ -18,6 +22,10 @@ const V2 = ({ currentUser }) => (
           <Route path="/spots/:id" component={SpotsShow} />
           <Route path="/error" component={LoginVKError} />
           <Route path="/integrations/vk/actions/success" component={LoginVKSuccess} />
+
+          <SessionFetcher>
+            <Route path="/history" component={HistoryScreen} />
+          </SessionFetcher>
         </Switch>
       ) : (
         <BootingScreen />
@@ -29,5 +37,9 @@ const V2 = ({ currentUser }) => (
 const mapStateToProps = state => ({
   currentUser: currentUserObtainer(state),
 });
+
+V2.propTypes = {
+  currentUser: PropTypes.object,
+};
 
 export default connect(mapStateToProps)(V2);
